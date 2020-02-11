ac22a1601947134289cf50f0c2074dad25184671
go <go05sato@gmail.com>
2020-02-11 22:12:25 +0900

    ARCameraにCameraResolutionScalerを追加 解像度が高いとNDIの遅延がひどい為

---
+++ Assets/_haquxx/CameraResolutionScaler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraResolutionScaler : MonoBehaviour
+{
+    [Range(0.01f, 1.0f)]
+    [SerializeField]
+    private float _renderScale = 1.0f;
+
+    [SerializeField]
+    private FilterMode _filterMode = FilterMode.Bilinear;
+
+    private Rect _originalRect;
+    private Rect _scaledRect;
+
+    private void OnDestroy()
+    {
+        Camera.main.rect = _originalRect;
+    }
+
+    private void OnPreRender()
+    {
+        _originalRect = Camera.main.rect;
+        _scaledRect.Set(_originalRect.x, _originalRect.y, _originalRect.width * _renderScale, _originalRect.height * _renderScale);
+        Camera.main.rect = _scaledRect;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Camera.main.rect = _originalRect;
+        source.filterMode = _filterMode;
+        Graphics.Blit(source, destination);
+    }
+}

8582d0951e29a176a0273239a969b7ee03f0271f
go|haquxx <go05sato@gmail.com>
2020-06-03 17:25:02 +0900

    Update test scene and settings

---
+++ Assets/_haquxx/Debug/DistanceChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class DistanceChecker : MonoBehaviour
+{
+    [SerializeField]
+    private TextMesh _text;
+
+    private void Update()
+    {
+        var cameraPos = Camera.main.transform.position;
+        cameraPos.y = transform.position.y;
+        var distance = Vector3.Distance(transform.position, cameraPos);
+        _text.text = distance.ToString();
+    }
+}

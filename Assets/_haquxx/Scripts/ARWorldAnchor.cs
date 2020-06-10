fb113f9d68fe57cc6aa9cc45b967b48f9119bc44
go|haquxx <go05sato@gmail.com>
2020-06-10 15:12:33 +0900

    Refactoring ARWorldAnchor

---
+++ Assets/_haquxx/Scripts/ARWorldAnchor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+namespace ARVJ
+{
+    [RequireComponent(typeof(ARSessionOrigin))]
+    [RequireComponent(typeof(ARRaycastManager))]
+    public class ARWorldAnchor : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _arAnchor;
+        [SerializeField]
+        private Text _statusText;
+
+        private ARSessionOrigin _arSessionOrigin;
+        private ARRaycastManager _arRaycastManger;
+        private bool _isLocked = false;
+        private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        private Quaternion _rotation;
+        public Quaternion Rotation
+        {
+            get { return _rotation; }
+            set
+            {
+                _rotation = value;
+                if (_arSessionOrigin != null)
+                    _arSessionOrigin.MakeContentAppearAt(_arAnchor, _arAnchor.transform.position, _rotation);
+            }
+        }
+
+        private void Awake()
+        {
+            _arSessionOrigin = GetComponent<ARSessionOrigin>();
+            _arRaycastManger = GetComponent<ARRaycastManager>();
+        }
+
+        private void Update()
+        {
+            if (_isLocked) return;
+
+            if (_arRaycastManger.Raycast(new Vector2(Screen.width / 2.0f, Screen.height / 2.0f), hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose pose = hits[0].pose;
+
+                // Move ARSessionOrigin, not content
+                _arSessionOrigin.MakeContentAppearAt(_arAnchor, pose.position, _rotation);
+            }
+        }
+
+        public void LockAnchor()
+        {
+            _isLocked = true;
+            _statusText.text = "Locked";
+            _statusText.color = Color.red;
+        }
+
+        public void UnlockAnchor()
+        {
+            _isLocked = false;
+            _statusText.text = "Unlocked";
+            _statusText.color = Color.green;
+        }
+    }
+}

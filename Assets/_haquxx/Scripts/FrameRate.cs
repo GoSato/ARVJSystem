dbcee89b2545c86da07de02d38a6840e5bb1571a
go|haquxx <go05sato@gmail.com>
2020-06-11 18:34:26 +0900

    Change the frame rate to 30

---
+++ Assets/_haquxx/Scripts/FrameRate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    [SerializeField]
+    private int _frameRate = 30;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = _frameRate;
+    }
+}

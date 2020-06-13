056a4a255fb3d9e77d2a4c264769ea95dda73db1
go|haquxx <go05sato@gmail.com>
2020-06-13 09:29:27 +0900

    Add Scene switch function

---
+++ Assets/_haquxx/Scripts/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private int _currentIndex = -1;
+
+    public void LoadScene(int val)
+    {
+        if (val == _currentIndex)
+            return;
+        
+        Debug.LogFormat("Load new content : {0}", val);
+        SceneManager.LoadScene(val, LoadSceneMode.Additive);
+
+        if (_currentIndex != -1)
+        {
+            SceneManager.UnloadSceneAsync(_currentIndex);
+        }
+        _currentIndex = val;  
+    }
+}

08c5ddf60e5f1e8e624878d15bd08d48d3932358
go|haquxx <go05sato@gmail.com>
2020-06-18 11:11:14 +0900

    Supports multiple scenes

--- Assets/_haquxx/Scripts/SceneSwitcher.cs
+++ Assets/_haquxx/Scripts/SceneSwitcher.cs
@@ -8,6 +8,7 @@ using System;
 public class SceneSwitcher : MonoBehaviour
 {
     private int _currentIndex = -1;
+    private int[] _activeSceneList = new int[3];
 
     [SerializeField]
     private string _oscAddress;
@@ -31,6 +32,37 @@ public class SceneSwitcher : MonoBehaviour
         if (_logText != null)
         {
             _logText.text = "[" + DateTime.Now + "]" + " : " + val;
+        }
+    }
+
+    public void LoadScene(Vector3Int indexList)
+    {
+        Debug.LogFormat("Load new content : {0},{1},{2}", indexList.x, indexList.y, indexList.z);
+
+        var tempList = new int[3] { indexList.x, indexList.y, indexList.z };
+
+        for (int i = 0; i < _activeSceneList.Length; i++)
+        {
+            if (Array.IndexOf(tempList, _activeSceneList[i]) == -1 && _activeSceneList[i] != 0)
+            {
+                SceneManager.UnloadSceneAsync(_activeSceneList[i]);
+            }
+        }
+
+        for (int i = 0; i < tempList.Length; i++)
+        {
+            var index = Array.IndexOf(_activeSceneList, tempList[i]);
+            if (index == -1 && tempList[i] != 0)
+            {
+                SceneManager.LoadScene(tempList[i], LoadSceneMode.Additive);
+            }
+        }
+
+        _activeSceneList = new int[3] { indexList.x, indexList.y, indexList.z };
+
+        if (_logText != null)
+        {
+            _logText.text = string.Format("[{0}] : {1},{2},{3}", DateTime.Now, indexList.x, indexList.y, indexList.z);
         }
     }
 }

a901b014359c548e150491a9979df8e73ac6d33d
go|haquxx <go05sato@gmail.com>
2020-08-03 02:15:00 +0900

    Add Refraction Shaders

---
+++ Assets/_haquxx/Contents/Refraction/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Vector3 _axis;
+    [SerializeField] private float _speed = 10f;
+
+    private void Update()
+    {
+        transform.Rotate(_axis, _speed * Time.deltaTime);
+    }
+}
